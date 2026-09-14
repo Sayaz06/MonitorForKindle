0823f1ca4d008f2913460c469f97a3770f4065bb
Sayaz06 <adha86614@gmail.com>
2026-09-14 15:57:23 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const VERSI = 'v1';
+const CACHE_NAME = `monitor-kindle-${VERSI}`;
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (!event.request.url.startsWith('http')) return;
+  event.respondWith(
+    fetch(event.request).then(response => {
+      if (response && response.status === 200 && response.type === 'basic') {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+      }
+      return response;
+    }).catch(() => caches.match(event.request).then(c => c || caches.match('./index.html')))
+  );
+});
